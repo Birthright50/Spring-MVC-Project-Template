138933d334494f9b600db2352bbe974b6fe586ea
Birthright50 <birthright5050@gmail.com>
2017-10-12 21:08:04 +0300

    test

--- src/main/webapp/resources/js/include_csrf.js
+++ src/main/webapp/resources/js/include_csrf.js
@@ -1,6 +1,3 @@
-/**
- * Created by birth on 18.02.2017.
- */
 $(function () {
     var token = $("meta[name='_csrf']").attr("content");
     var header = $("meta[name='_csrf_header']").attr("content");
